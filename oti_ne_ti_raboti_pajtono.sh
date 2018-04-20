78bfef66873b243e1c50bec9096ba67091c26d5a
Petar Gjorgievski <perozawow@gmail.com>
2018-04-20 11:14:58 +0200

    moded shell script

--- oti_ne_ti_raboti_pajtono.sh
+++ oti_ne_ti_raboti_pajtono.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 SITESFILE=sites.txt 
-
+timestamp=$(date +%T)
 while read site; do
     if [ ! -z "${site}" ]; then
-        
         CURL=$(curl -s --head $site)
-        
         if echo $CURL | grep "200 OK" > /dev/null
         then
+            echo $timestamp
             echo "The HTTP server on ${site} is up!"
         else    
             MESSAGE="This is an alert that your site ${site} has failed to respond 200 OK." 
