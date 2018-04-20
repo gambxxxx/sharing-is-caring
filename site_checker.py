924c2cb24367fff368ab82d507669dc399f7c6f8
Petar Gjorgievski <perozawow@gmail.com>
2018-04-20 16:58:44 +0200

    ne znam

--- site_checker.py
+++ site_checker.py
@@ -3,11 +3,10 @@ import time
 import colorama
 from colorama import Fore
 from colorama import Style
-
 def check_uptime():
-    t=2 
+    t=300 
     web_site_url="http://allutomotive.com/"
-    web_site_name="Allutomotive.com"
+    web_site_name="Allutomotive"
     while True:
         try:
             print(urllib.request.urlopen(web_site_url).getcode())
