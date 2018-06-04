9dd9a5bcffe0710bfffae34438cad015ffd137a0
Petar Gjorgievski <perozawow@gmail.com>
2018-06-04 14:39:40 +0200

    added sleep

--- playbook-bundle.sh
+++ playbook-bundle.sh
@@ -2,7 +2,7 @@
 #
 #
 echo "Installing Ansible ..."
-
+sleep 2
 sudo apt-get update
 sudo apt-get install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
@@ -14,5 +14,6 @@ sleep 2
 ansible-playbook -i hosts -K installer-playbook.yml
 
 echo "Generating Ssh-key in ${$HOME/.ssh}..."
+sleep 2
 ssh-keygen -t rsa -b 2048 -N "" -f $HOME/.ssh
 sleep 2
